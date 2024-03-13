561ab400465df2bdb4ef3aade356d872cd4035be
Abhijeet Chakravorty <chakravorty.abhijeet91@gmail.com>
2024-03-13 08:31:06 +0530

    updated code for testing

--- src/test/server_app/data/Database.test.ts
+++ src/test/server_app/data/Database.test.ts
@@ -11,6 +11,18 @@ describe("Database test suite", () => {
 
     const fakeId = "1234";
 
+    const someObject1 = {
+        id: "",
+        name: "someName",
+        color: "blue",
+    };
+
+    const someObject2 = {
+        id: "",
+        name: "someOtherName",
+        color: "blue",
+    };
+
     beforeEach(() => {
         sut = new DataBase<someTypeWithId>();
         jest.spyOn(IdGenerator, "generateRandomId").mockReturnValue(fakeId);
@@ -21,5 +33,21 @@ describe("Database test suite", () => {
             id: "",
         } as any);
         expect(actual).toBe(fakeId);
+    });
+
+    it("should get elements after insert", async () => {
+        const id = await sut.insert(someObject1);
+        const actual = await sut.getBy("id", id);
+        expect(actual).toBe(someObject1);
+    });
+
+    it("should find all elements with the same property", async () => {
+        await sut.insert(someObject1);
+        await sut.insert(someObject2);
+
+        const expected = [someObject1, someObject2];
+
+        const actual = await sut.findAllBy("color", "blue");
+        expect(actual).toEqual(expected);
     });
 });
